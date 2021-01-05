3bb527b9097fd23a4b05337f75e155d2107e6e23
Raphael Hessel <raphahessel@gmail.com>
2021-01-04 21:12:18 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -43,5 +43,14 @@ public class OrderService {
 		return new OrderDTO(order);
 		
 	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
+		
+	}
 }
 
