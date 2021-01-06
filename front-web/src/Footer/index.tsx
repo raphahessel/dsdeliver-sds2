585d56202c781b46f7c602dcfad58bdf9eaa5ddc
Raphael Hessel <raphahessel@gmail.com>
2021-01-06 16:52:09 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css'
+import {ReactComponent as YouTubeIcon} from './youtube.svg'
+import {ReactComponent as LinkedinIcon} from './linkedin.svg'
+import {ReactComponent as InstagramIcon} from './instagram.svg'
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido 
+            durante a 2ª ed. do 
+            evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="" target="_new">
+                    <YouTubeIcon></YouTubeIcon>
+                </a>
+                <a href="" target="_new">
+                    <LinkedinIcon></LinkedinIcon>
+                </a>
+                <a href="" target="_new">
+                    <InstagramIcon></InstagramIcon>
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
