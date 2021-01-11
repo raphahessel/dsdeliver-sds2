f6e707ab9a2053c2a52c82fd1a211ca625351fd3
Raphael Hessel <raphahessel@gmail.com>
2021-01-10 22:24:32 -0300

    Finish Front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css';
-import Navbar from './Navbar';
-import Routes from './Routes';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -10,13 +10,13 @@ function Footer(){
             durante a 2ª ed. do 
             evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="" target="_new">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
                     <YouTubeIcon></YouTubeIcon>
                 </a>
-                <a href="" target="_new">
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
                     <LinkedinIcon></LinkedinIcon>
                 </a>
-                <a href="" target="_new">
+                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
                     <InstagramIcon></InstagramIcon>
                 </a>
             </div>
